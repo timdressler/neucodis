2593de075057ac29f2b43a265311bfadd32da028
Tim Dreßler <tim.dressler@uni-oldenburg.de>
2024-09-05 11:15:06 +0200

    Update pp_gamma_pretest.m

--- scripts/pp_gamma_pretest.m
+++ scripts/pp_gamma_pretest.m
@@ -94,7 +94,7 @@ end
 
 %Topoplot
 figure;
-tftopo(allersp,alltimes(:,:,1),allfreqs(:,:,1), ...
+ftopo(allersp,alltimes(:,:,1),allfreqs(:,:,1), ...
     'timefreqs', [45 35; 55 38], 'chanlocs', EEG.chanlocs, 'showchan', chani)
 
 
