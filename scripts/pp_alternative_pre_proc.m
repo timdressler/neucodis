998534c9d34a0aa59e0b405ed96c43621887bd9e
Tim Dreßler <tim.dressler@uni-oldenburg.de>
2024-10-14 14:44:16 +0200

    Update PreProcessing
    loop over subjects & check if more than one condition file exists

--- scripts/pp_alternative_pre_proc.m
+++ scripts/pp_alternative_pre_proc.m
@@ -14,133 +14,169 @@ MAINPATH = "C:/Users/timdr/OneDrive/Uni_Oldenburg/3_Semester\Module/Pratical_Pro
 INPATH = fullfile(MAINPATH,"data/raw_data/pp_main_data_raw/");
 addpath("C:/Users/timdr/OneDrive/Uni_Oldenburg/3_Semester\Module/Pratical_Project/Analysis/neucodis/functions")
 
+%variables to edit
+EVENTS = {'talk', 'listen'};
+EPO_FROM = -0.8;
+EPO_TILL = 0.7;
+LCF = 1;
+HCF = 60;
+LCF_2 = 20;
+HCF_2 = 55;
+BL_FROM = -800;
+BL_TILL= -400;
+TF_FROM = -800;
+TF_TILL = 600;
+TF_BL_FROM = -600;
+TF_BL_TILL = -400;
+THRESH = 100;
+SD_PROB = 3;
+
+%get directory content
+dircont_subj = dir(fullfile(INPATH, 'P*'));
+
+%initialize marked subjects variable
+MARKED_SUBJ = [];
+
 %% load and merge data
-%start eeglab
-[ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
+for subj = 1:length(dircont_subj)
+    %get current ID
+    SUBJ = dircont_subj(subj).name;
+    %check number of condition files
+    dircont_cond1 = dir(fullfile(INPATH, [SUBJ '/*C_0001.vhdr']));
+    dircont_cond2 = dir(fullfile(INPATH, [SUBJ '/*C_0005.vhdr']));
+    if length(dircont_cond1) == 1 && length(dircont_cond2) == 1
+        %start eeglab
+        [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
+        %preprocessing dataset C_0001
+        %load raw dataset (C_0001)
+        EEG = pop_loadbv(fullfile(INPATH, SUBJ), ['av_' SUBJ '_C_0001.vhdr'], [], []); %CHECK %channel locations already there
+        %remove not needed channels
+        EEG = pop_select( EEG, 'rmchannel',{'heogl','heogr','ml','mr', 'Lip'});
+        %rename events
+        for s = 2:length(EEG.event)
+            if s < length(EEG.event)
+                if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64') && ~strcmp(EEG.event(s+1).type, 'S 64')
+                    EEG.event(s).type = 'talk';
+                else
+                    continue
+                end
+            elseif s == length(EEG.event)
+                if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64')
+                    EEG.event(s).type = 'talk';
+                else
+                    continue
+                end
+            end
+        end
+        %1-60Hz bandpass
+        EEG = pop_eegfiltnew(EEG, 'locutoff',LCF,'hicutoff',HCF,'plotfreqz',0);
+        %run PREP pipeline
+        %detrend
+        [EEG, ~] = removeTrend(EEG);
+        %line noise removal
+        lineNoiseIn = getPrepDefaults(EEG, 'linenoise');
 
-%preprocessing dataset C_0001
-%load raw dataset (C_0001)
-EEG = pop_loadbv(fullfile(INPATH, 'P136'), 'av_P136_C_0001.vhdr', [], []);
-%remove not needed channels
-EEG = pop_select( EEG, 'rmchannel',{'heogl','heogr','ml','mr', 'Lip'});
-%rename events
-for s = 2:length(EEG.event)
-    if s < length(EEG.event)
-        if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64') && ~strcmp(EEG.event(s+1).type, 'S 64')
-            EEG.event(s).type = 'talk';
-        else
-            continue
+        lineNoiseIn.fPassBand = lineNoiseIn.fPassBand.value;
+        lineNoiseIn.Fs = lineNoiseIn.Fs.value;
+        lineNoiseIn.fScanBandWidth = lineNoiseIn.fScanBandWidth.value;
+        lineNoiseIn.lineFrequencies = 50:50:EEG.srate/2;
+        lineNoiseIn.lineNoiseChannels = lineNoiseIn.lineNoiseChannels.value;
+        lineNoiseIn.lineNoiseMethod = lineNoiseIn.lineNoiseMethod.value;
+        lineNoiseIn.maximumIterations = lineNoiseIn.maximumIterations.value;
+        lineNoiseIn.p = lineNoiseIn.p.value;
+        lineNoiseIn.pad = lineNoiseIn.pad.value;
+        lineNoiseIn.taperBandWidth = lineNoiseIn.taperBandWidth.value;
+        lineNoiseIn.taperWindowSize = lineNoiseIn.taperWindowSize.value;
+        lineNoiseIn.taperWindowStep = lineNoiseIn.taperWindowStep.value;
+        lineNoiseIn.tau = lineNoiseIn.tau.value;
+
+        [EEG, lineNoiseOut] = cleanLineNoise(EEG, lineNoiseIn);
+        %robust rereferencing
+        [EEG, referenceOut] = performReference(EEG);
+        %store dataset C_0001
+        [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
+
+        %preprocessing dataset C_0005
+        %load raw dataset (C_0005)
+        EEG = pop_loadbv(fullfile(INPATH, SUBJ), ['av_' SUBJ '_C_0005.vhdr'], [], []); %CHECK %channel locations already there
+        %remove not needed channels
+        EEG = pop_select( EEG, 'rmchannel',{'heogl','heogr','ml','mr', 'Lip'});
+        %rename events
+        for s = 2:length(EEG.event)
+            if s < length(EEG.event)
+                if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64') && ~strcmp(EEG.event(s+1).type, 'S 64')
+                    EEG.event(s).type = 'listen';
+                else
+                    continue
+                end
+            elseif s == length(EEG.event)
+                if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64')
+                    EEG.event(s).type = 'listen';
+                else
+                    continue
+                end
+            end
         end
-    elseif s == length(EEG.event)
-        if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64')
-            EEG.event(s).type = 'talk';
-        else
-            continue
-        end
+        %1-60Hz bandpass
+        EEG = pop_eegfiltnew(EEG, 'locutoff',LCF,'hicutoff',HCF,'plotfreqz',0);
+        %run PREP pipeline
+        %detrend
+        [EEG, ~] = removeTrend(EEG);
+        %line noise removal
+        lineNoiseIn = getPrepDefaults(EEG, 'linenoise');
+
+        lineNoiseIn.fPassBand = lineNoiseIn.fPassBand.value;
+        lineNoiseIn.Fs = lineNoiseIn.Fs.value;
+        lineNoiseIn.fScanBandWidth = lineNoiseIn.fScanBandWidth.value;
+        lineNoiseIn.lineFrequencies = 50:50:EEG.srate/2;
+        lineNoiseIn.lineNoiseChannels = lineNoiseIn.lineNoiseChannels.value;
+        lineNoiseIn.lineNoiseMethod = lineNoiseIn.lineNoiseMethod.value;
+        lineNoiseIn.maximumIterations = lineNoiseIn.maximumIterations.value;
+        lineNoiseIn.p = lineNoiseIn.p.value;
+        lineNoiseIn.pad = lineNoiseIn.pad.value;
+        lineNoiseIn.taperBandWidth = lineNoiseIn.taperBandWidth.value;
+        lineNoiseIn.taperWindowSize = lineNoiseIn.taperWindowSize.value;
+        lineNoiseIn.taperWindowStep = lineNoiseIn.taperWindowStep.value;
+        lineNoiseIn.tau = lineNoiseIn.tau.value;
+
+        [EEG, lineNoiseOut] = cleanLineNoise(EEG, lineNoiseIn);
+        %robust rereferencing
+        [EEG, referenceOut] = performReference(EEG);
+        %store dataset C_0005
+        [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
+
+        %preprocessing on merged datasets
+        %merge datasets
+        EEG = pop_mergeset( ALLEEG, [1 2], 0);
+        [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
+        %run ICA
+        EEG = pop_runica(EEG, 'icatype', 'runica', 'extended',1,'interrupt','on');
+        %label ICA components with IC Label Plugin
+        EEG = pop_iclabel(EEG, 'default');
+        EEG = pop_icflag(EEG, [0 0.2;0.9 1;0.9 1;0.9 1;0.9 1;0.9 1;0.9 1]);
+        EEG = pop_subcomp( EEG, [], 0);
+        %compute surface laplacian
+        EEG.data=laplacian_perrinX(EEG.data,EEG.chanlocs.X,EEG.chanlocs.Y,EEG.chanlocs.Z);
+        %20-55Hz bandpass
+        EEG = pop_eegfiltnew(EEG, 'locutoff',LCF_2,'hicutoff',LCF_2,'plotfreqz',0);
+        %epoching
+        EEG = pop_epoch( EEG, {  'talk'  }, [EPO_FROM        EPO_TILL], 'epochinfo', 'yes');
+        %baseline removal
+        EEG = pop_rmbase( EEG, [BL_FROM BL_TILL] ,[]);
+        %threshold removal
+        EEG = pop_eegthresh(EEG,1,[1:EEG.nbchan] ,-THRESH,THRESH,-0.8,0.699,2,1);
+        EEG = pop_eegthresh(EEG,1,[1:EEG.nbchan] ,-THRESH,THRESH,-0.8,0.699,2,0);
+        %probability-based removal
+        EEG = pop_jointprob(EEG,1,[1:EEG.nbchan] ,SD_PROB,0,0,0,[],0);
+        EEG = pop_rejkurt(EEG,1,[1:EEG.nbchan] ,SD_PROB,0,0,0,[],0);
+        EEG = eeg_rejsuperpose( EEG, 1, 1, 1, 1, 1, 1, 1, 1);
+        EEG = pop_rejepoch( EEG, EEG.reject.rejglobal ,0);
+        %end of preprocessing
+
+    else
+        MARKED_SUBJ(end+1) = SUBJ;
     end
 end
-%1-60Hz bandpass
-EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',60,'plotfreqz',0);
-%run PREP pipeline
-%detrend
-[EEG, ~] = removeTrend(EEG);
-%line noise removal
-lineNoiseIn = getPrepDefaults(EEG, 'linenoise');
-
-lineNoiseIn.fPassBand = lineNoiseIn.fPassBand.value;
-lineNoiseIn.Fs = lineNoiseIn.Fs.value;
-lineNoiseIn.fScanBandWidth = lineNoiseIn.fScanBandWidth.value;
-lineNoiseIn.lineFrequencies = 50:50:EEG.srate/2;
-lineNoiseIn.lineNoiseChannels = lineNoiseIn.lineNoiseChannels.value;
-lineNoiseIn.lineNoiseMethod = lineNoiseIn.lineNoiseMethod.value;
-lineNoiseIn.maximumIterations = lineNoiseIn.maximumIterations.value;
-lineNoiseIn.p = lineNoiseIn.p.value;
-lineNoiseIn.pad = lineNoiseIn.pad.value;
-lineNoiseIn.taperBandWidth = lineNoiseIn.taperBandWidth.value;
-lineNoiseIn.taperWindowSize = lineNoiseIn.taperWindowSize.value;
-lineNoiseIn.taperWindowStep = lineNoiseIn.taperWindowStep.value;
-lineNoiseIn.tau = lineNoiseIn.tau.value;
-
-[EEG, lineNoiseOut] = cleanLineNoise(EEG, lineNoiseIn);
-%robust rereferencing
-[EEG, referenceOut] = performReference(EEG);
-%store dataset C_0001
-[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
-
-%preprocessing dataset C_0005
-%load raw dataset (C_0005)
-EEG = pop_loadbv(fullfile(INPATH, 'P136'), 'av_P136_C_0001.vhdr', [], []);
-%remove not needed channels
-EEG = pop_select( EEG, 'rmchannel',{'heogl','heogr','ml','mr', 'Lip'});
-%rename events
-for s = 2:length(EEG.event)
-    if s < length(EEG.event)
-        if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64') && ~strcmp(EEG.event(s+1).type, 'S 64')
-            EEG.event(s).type = 'listen';
-        else
-            continue
-        end
-    elseif s == length(EEG.event)
-        if strcmp(EEG.event(s).type, 'S 64') && ~strcmp(EEG.event(s-1).type, 'S 64')
-            EEG.event(s).type = 'listen';
-        else
-            continue
-        end
-    end
-end
-%1-60Hz bandpass
-EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',60,'plotfreqz',0);
-%run PREP pipeline
-%detrend
-[EEG, ~] = removeTrend(EEG);
-%line noise removal
-lineNoiseIn = getPrepDefaults(EEG, 'linenoise');
-
-lineNoiseIn.fPassBand = lineNoiseIn.fPassBand.value;
-lineNoiseIn.Fs = lineNoiseIn.Fs.value;
-lineNoiseIn.fScanBandWidth = lineNoiseIn.fScanBandWidth.value;
-lineNoiseIn.lineFrequencies = 50:50:EEG.srate/2;
-lineNoiseIn.lineNoiseChannels = lineNoiseIn.lineNoiseChannels.value;
-lineNoiseIn.lineNoiseMethod = lineNoiseIn.lineNoiseMethod.value;
-lineNoiseIn.maximumIterations = lineNoiseIn.maximumIterations.value;
-lineNoiseIn.p = lineNoiseIn.p.value;
-lineNoiseIn.pad = lineNoiseIn.pad.value;
-lineNoiseIn.taperBandWidth = lineNoiseIn.taperBandWidth.value;
-lineNoiseIn.taperWindowSize = lineNoiseIn.taperWindowSize.value;
-lineNoiseIn.taperWindowStep = lineNoiseIn.taperWindowStep.value;
-lineNoiseIn.tau = lineNoiseIn.tau.value;
-
-[EEG, lineNoiseOut] = cleanLineNoise(EEG, lineNoiseIn);
-%robust rereferencing
-[EEG, referenceOut] = performReference(EEG);
-%store dataset C_0005
-[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
-
-%preprocessing on merged datasets
-%merge datasets
-EEG = pop_mergeset( ALLEEG, [1 2], 0);
-[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
-%run ICA
-EEG = pop_runica(EEG, 'icatype', 'runica', 'extended',1,'interrupt','on');
-%label ICA components with IC Label Plugin
-EEG = pop_iclabel(EEG, 'default');
-EEG = pop_icflag(EEG, [0 0.2;0.9 1;0.9 1;0.9 1;0.9 1;0.9 1;0.9 1]);
-EEG = pop_subcomp( EEG, [], 0);
-%compute surface laplacian
-EEG.data=laplacian_perrinX(EEG.data,EEG.chanlocs.X,EEG.chanlocs.Y,EEG.chanlocs.Z);
-%20-55Hz bandpass
-EEG = pop_eegfiltnew(EEG, 'locutoff',20,'hicutoff',55,'plotfreqz',0);
-%epoching
-EEG = pop_epoch( EEG, {  'talk'  }, [-0.8         0.7], 'epochinfo', 'yes');
-%baseline removal
-EEG = pop_rmbase( EEG, [-800 -400] ,[]);
-%threshold removal
-EEG = pop_eegthresh(EEG,1,[1:60] ,-50,50,-0.8,0.699,2,1); %CHECK %changed from +/- 100 to +/- 50
-%probability-based removal
-EEG = pop_jointprob(EEG,1,[1:60] ,3,3,0,1,0,[],0);
-EEG = pop_rejkurt(EEG,1,[1:60] ,3,3,0,1,0,[],0);
-
-%end of preprocessiing
 
 eeglab redraw
 
