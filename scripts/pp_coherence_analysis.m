d3b2db91161d3e5eab8b3c92bc996b1f9c27df6f
Tim Dreßler <tim.dressler@uni-oldenburg.de>
2024-11-04 16:28:22 +0100

    Major Update Coherence Analysis
    ft_freqstatistics runs :)

--- scripts/pp_coherence_analysis.m
+++ scripts/pp_coherence_analysis.m
@@ -26,6 +26,12 @@ FUNPATH = fullfile(MAINPATH, 'neucodis\functions\');
 addpath(FUNPATH);
 
 %variables to edit
+WPLI_BL_FROM = -0.4;
+WPLI_BL_TILL = -0.3;
+TF_F_FROM = 25;
+TF_F_TILL = 50;
+TF_T_FROM = -0.4;
+TF_T_TILL = 0.2;
 %selection of electrodes (left)
 FRONTAL_L = {'F3', 'F5', 'F7', 'Fp1'};
 TEMPORAL_L = {'T7', 'TP7', 'P7', 'FC5'};
@@ -80,7 +86,7 @@ ok_subj = {};
 
 clear pairs
 r = 1;
-for pairs = 1:length(all_pairs) %loop over electode pair-sets
+for pairs = 1:length(all_pairs) %loop over electrode pair-sets
     r_start = r;
     clear subj subj_time subj_check
     for subj = 1:length(dircont_subj) %loop over subjects
@@ -98,7 +104,7 @@ for pairs = 1:length(all_pairs) %loop over electode pair-sets
         %rename dataset
         EEG.setname = [SUBJ '_talk_listen'];
         [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
-        
+
         %rename dataset
         EEG.setname = [SUBJ '_talk_listen'];
         [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
@@ -125,8 +131,8 @@ for pairs = 1:length(all_pairs) %loop over electode pair-sets
             cfg_freq.output = 'powandcsd';
             cfg_freq.channel = {all_pairs(pairs).pairs{elec_pair,1}, all_pairs(pairs).pairs{elec_pair,2}};
             cfg_freq.keeptrials = 'yes';
-            cfg_freq.foi = 25:1:50;
-            cfg_freq.toi = -0.4:0.01:0.2;
+            cfg_freq.foi = TF_F_FROM:1:TF_F_TILL;
+            cfg_freq.toi = TF_T_FROM:0.01:TF_T_TILL;
             cfg_freq.width = 6;
 
             %TF analysis for talk condition
@@ -138,20 +144,26 @@ for pairs = 1:length(all_pairs) %loop over electode pair-sets
             %setup
             cfg_conn = [];
             cfg_conn.method = 'wpli_debiased';
-            % % cfg_conn.channelcmb = PAIRS_L;  % Paar 2: C3 - C4
             cfg_conn.keeptrials = 'yes';
 
             %connectivity analysis for talk condition
             wpli_talk = ft_connectivityanalysis(cfg_conn, freq_talk);
-            %baseline correction
-
+            %extract wpli values
             wpli_talk_extracted = squeeze(wpli_talk.wpli_debiasedspctrm);
+            %convert latencies to samples
+            [~, wpli_bl_start_sam] = min(abs(wpli_talk.time-WPLI_BL_FROM));
+            [~, wpli_bl_end_sam] = min(abs(wpli_talk.time-WPLI_BL_TILL));
+            %baseline correction
+            % % wpli_talk_extracted = wpli_talk_extracted - mean(wpli_talk_extracted(:,wpli_bl_start_sam:wpli_bl_end_sam),2);
             %connectivity analysis for listen condition
             wpli_listen = ft_connectivityanalysis(cfg_conn, freq_listen);
-            %baseline correction
-            
+            %extract wpli values
             wpli_listen_extracted = squeeze(wpli_listen.wpli_debiasedspctrm);
-
+            %convert latencies to samples
+            [~, wpli_bl_start_sam] = min(abs(wpli_talk.time-WPLI_BL_FROM));
+            [~, wpli_bl_end_sam] = min(abs(wpli_talk.time-WPLI_BL_TILL));
+            %baseline correction
+            % % wpli_talk_extracted = wpli_talk_extracted - mean(wpli_talk_extracted(:,wpli_bl_start_sam:wpli_bl_end_sam),2);
             %store wPLI values over all pairs
             wpli_talk_extracted_ALL_PAIRS(:,:,elec_pair) = wpli_talk_extracted;
             wpli_listen_extracted_ALL_PAIRS(:,:,elec_pair) = wpli_listen_extracted;
@@ -199,16 +211,37 @@ for pairs = 1:length(all_pairs) %loop over electode pair-sets
     all_wpli(pairs).talk_GA = wpli_talk_GRANDAVERAGE;
     all_wpli(pairs).listen_GA = wpli_listen_GRANDAVERAGE;
 
-    % % cfg = [];
-    % % cfg.method           = 'montecarlo';
-    % % cfg.statistic        = 'ft_statfun_depsamplesT';
-    % % cfg.correctm         = 'cluster';
-    % % cfg.clusteralpha     = 0.05;
-    % % cfg.clusterstatistic = 'maxsum';
-    % % cfg.alpha            = 0.025;
-    % % cfg.numrandomization = 500;
+    % % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    cfg = [];
+    cfg.method            = 'montecarlo';           % use the Monte Carlo Method to calculate the significance probability
+    cfg.statistic         = 'depsamplesT';        % use the independent samples T-statistic as a measure to evaluate the effect at the sample level
+    cfg.correctm          = 'cluster';
+    cfg.clusteralpha      = 0.05;                   % alpha level of the sample-specific test statistic that will be used for thresholding
+    cfg.clustertail       = 0;
+    cfg.clusterstatistic  = 'maxsum';               % test statistic that will be evaluated under the permutation distribution.
+    cfg.alpha             = 0.05;                   % alpha level of the permutation test
+    cfg.numrandomization  = 1000;                   % number of draws from the permutation distribution
+    cfg.ivar              = 1;                      % the index of the independent variable in the design matrix
+    cfg.neighbours        = [];                     % there are no spatial neighbours, only in time and frequency
+    cfg.tail              = 0;                      % -1, 1 or 0 (default = 0); one-sided or two-sided test
+    cfg.parameter = 'wpli_debiasedspctrm';
 
-    % % all_wpli(pairs).comparison = ft_freqstatistics(cfg,all_wpli(pairs).talk_GA,all_wpli(pairs).listen_GA);
+    subj_design = length(dircont_subj);
+    design = zeros(2,2*subj_design);
+    for i = 1:subj_design
+        design(1,i) = i;
+    end
+    for i = 1:subj_design
+        design(1,subj_design+i) = i;
+    end
+    design(2,1:subj_design)        = 1;
+    design(2,subj_design+1:2*subj_design) = 2;
+
+    cfg.design   = design;
+    cfg.uvar     = 1;
+    cfg.ivar     = 2;
+    % % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    all_wpli(pairs).comparison = ft_freqstatistics(cfg,all_wpli(pairs).talk_GA,all_wpli(pairs).listen_GA);
 
 
     %sanity checks
@@ -225,7 +258,7 @@ check_done = 'OK'
 time_vector = wpli_listen.time;
 freq_vector = wpli_listen.freq;
 figure;
-imagesc(time_vector, freq_vector, squeeze(mean(wpli_listen.wpli_debiasedspctrm, 1)));
+imagesc(time_vector, freq_vector, wpli_listen_extracted);
 axis xy;
 xlabel('Zeit (s)');
 ylabel('Frequenz (Hz)');
@@ -236,7 +269,7 @@ caxis([0, max(max(squeeze(mean(wpli_listen.wpli_debiasedspctrm, 1))))]);
 time_vector = wpli_talk.time;
 freq_vector = wpli_talk.freq;
 figure;
-imagesc(time_vector, freq_vector, squeeze(mean(wpli_talk.wpli_debiasedspctrm, 1)));
+imagesc(time_vector, freq_vector,wpli_talk_extracted);
 axis xy;
 xlabel('Zeit (s)');
 ylabel('Frequenz (Hz)');
